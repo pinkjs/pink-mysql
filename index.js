d3d2a20ccaaf17ef395a80ea63840b84c2ff9001
zhoujun <zhoujun247@foxmail.com>
2017-05-04 11:42:33 +0800

    update mysql Class

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 /**
  * Created by zhoujun on 2017/4/17.
  */
+module.exports = require('./src/mysql');
--- src/mysql.js
+++ src/mysql.js
@@ -1,3 +1,52 @@
 /**
  * Created by zhoujun on 2017/4/17.
  */
+'use strict'
+const mysql = require('mysql');
+
+Promise = require('bluebird');
+
+const Pool  = require('mysql/lib/Pool');
+
+const Connection = require('mysql/lib/Connection');
+
+Promise.promisifyAll([Pool,Connection]);
+
+const model = Object.create(mysql);
+
+
+/*
+* 连接数据库的方法
+* */
+model.connection = function ( config ) {
+	this.pool = model.createPool(config);
+}
+
+model.query = async function ( sql,value ) {
+
+	return await this.pool.queryAsync(sql,value);
+}
+
+model._create = async function (value,tableName ) {
+	let sql = `insert into ${tableName} SET ?`;
+	let result = await this.query(sql,value);
+	return this;
+}
+model.find = async function ( select = '*', tableName ) {
+	this.sql = `select ? from ${tableName}`;
+	return this;
+}
+
+class Mysql {
+	constructor(){
+
+	}
+	create(values){
+		return model._create(values,this.tableName)
+	}
+	find(select){
+		model.find(select,this.tableName);
+	}
+}
+
+module.exports = Mysql;
