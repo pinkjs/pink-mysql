e68ec6f683b0c258d1c394c3ee4ea2718744701b
zhoujun <zhoujun247@foxmail.com>
2017-05-05 15:05:02 +0800

    暂时不提交测试

--- test/mysql.js
+++ test/mysql.js
@@ -6,8 +6,8 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Mysql = require('../src/mysql');
 
-describe('mysql', function() {
-	it('find() should return 0 if no items are passed in', function() {
-
-	});
-});
+//describe('Mysql', function() {
+//	it('find() should return 0 if no items are passed in', function() {
+//
+//	});
+//});
